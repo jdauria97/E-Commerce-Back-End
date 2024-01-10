3ffd3b50e4603f6711d2f83051e747c4c1e198f1
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-01-09 22:51:33 -0500

    fixes connection and .env

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.ecommerce_db,
-    process.env.root,
-    process.env.JLKIPOjlkipo809*
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
